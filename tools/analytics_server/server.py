567461499faaf1b19a0d530c6b0a905f16f32d48
Walter Moreira <walter@waltermoreira.net>
2014-08-12 15:52:27 -0500

    Add analytics server

---
+++ tools/analytics_server/server.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask.ext import restful
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+
+class Server(restful.Resource):
+
+    def get(self):
+        return {'hello': 'world'}
+
+    def post(self):
+        print('------')
+        print(request.get_json())
+
+
+api.add_resource(Server, '/')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
